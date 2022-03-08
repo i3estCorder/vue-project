3e5704ef702a20a2c02f96fe776d102f70fe8d1c
dongsuk.chun <ikarus1004@gmail.com>
2022-03-08 18:32:39 +0900

    8.1.5 부모 컴포넌트에서 자식 컴포넌트의 데이터 옵션 값 직접 변경하기

--- src/views/ChildComponent.vue
+++ src/views/ChildComponent.vue
@@ -1,11 +1,11 @@
 <template>
-    <button type="button" @click="childFunc" ref="btn">Click</button>
+    <h1>{{msg}}</h1>
 </template>
 <script>
 export default {
-    methods:{
-        childFunc(){
-            console.log("부모 컴포넌트에서 직접 발생시킨 이벤트")
+    data(){
+        return{
+            msg:''
         }
     }
 }
--- src/views/ParentComponent.vue
+++ src/views/ParentComponent.vue
@@ -1,13 +1,16 @@
 <template>
     <child-component @send-message="sendMessage" ref="child_component"></child-component>
+    <button type="button" @click="changeChildData">Change Child Data</button>
 </template>
 <script>
 import ChildComponent from './ChildComponent'
 
 export default {
     components: {ChildComponent},
-    mounted(){
-        this.$refs.child_component.childFunc()
+    methods:{
+        changeChildData(){
+            this.$refs.child_component.msg = '부모 컴포넌트가 변경한 데이터'
+        }
     }    
 }
 </script>
