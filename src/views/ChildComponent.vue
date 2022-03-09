2140d6df4159b99dc118bb2aa933781aaf19d498
dongsuk.chun <ikarus1004@gmail.com>
2022-03-09 13:44:45 +0900

    8.1.7 부모 컴포넌트에서 자식 컴포넌트의 데이터 옵션 값 동기화하기

--- src/views/ChildComponent.vue
+++ src/views/ChildComponent.vue
@@ -1,15 +1,17 @@
 <template>
-    <h1>{{msg}}</h1>
+    <button type="button" @click="childFunc" ref="btn">자식 컴포넌트 데이터 변경</button>
 </template>
 <script>
 export default {
     data(){
         return{
-            msg:'자식 컴포넌트로부터 보내는 메시지'
+            msg:'메시지'
         }
     },
-    mounted(){
-        this.$emit('send-message', this.msg)
+    methods:{
+        childFunc(){
+            this.msg = '변경된 메시지'
+        }
     }
 }
 </script>
--- src/views/ParentComponent.vue
+++ src/views/ParentComponent.vue
@@ -1,14 +1,20 @@
 <template>
-    <child-component @send-message="sendMessage"></child-component>
+    <button type="button" @click="checkChild">자식 컴포넌트 데이터 조화</button>
+    <child-component ref="child_component"></child-component>
 </template>
 <script>
 import ChildComponent from './ChildComponent'
 
 export default {
     components: {ChildComponent},
+    computed:{
+        msg(){
+            return this.$refs.child_component.msg
+        }
+    },
     methods:{
-        sendMessage(data){
-            console.log(data)
+        checkChild(){
+            alert(this.msg)
         }
     }    
 }
